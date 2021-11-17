756c3a238cea538756680c209e4bb64908c96cf6
Renan Miyamoto <renanmmiyamoto@gmail.com>
2021-11-17 09:41:22 -0300

    finalizando modulo 2 - reports generator

---
+++ reports_generator/lib/reports_generator.ex
@@ -0,0 +1,66 @@
+defmodule ReportsGenerator do
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options ["users", "foods"]
+
+  def build(filename) do
+    filename
+    |> ReportsGenerator.Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list of strings"}
+  end
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reporst(report, result) end)
+  end
+
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option"}
+
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users}) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    build_report(foods, users)
+  end
+
+  defp sum_reporst(%{"foods" => foods1, "users" => users1}, %{
+         "foods" => foods2,
+         "users" => users2
+       }) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+
+  defp report_acc do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    build_report(foods, users)
+  end
+
+  defp build_report(foods, users), do: %{"foods" => foods, "users" => users}
+end
